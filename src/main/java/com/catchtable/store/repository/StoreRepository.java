218b56c10e4b892f054ddc43af496e0b3af99976
silkair <silkair1234@gmail.com>
2026-04-15 17:03:27 +0900

    feat : review count 로직 구현

--- src/main/java/com/catchtable/store/repository/StoreRepository.java
+++ src/main/java/com/catchtable/store/repository/StoreRepository.java
@@ -3,6 +3,7 @@ package com.catchtable.store.repository;
 import com.catchtable.store.entity.District;
 import com.catchtable.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +15,12 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     List<Store> searchByStoreName(@Param("name") String name);
 
     List<Store> findAllByDistrictAndIsDeletedFalse(District district);
+
+    @Modifying
+    @Query("UPDATE Store s SET s.reviewCount = s.reviewCount + 1 WHERE s.id = :storeId")
+    void increaseReviewCount(@Param("storeId") Long storeId);
+
+    @Modifying
+    @Query("UPDATE Store s SET s.reviewCount = s.reviewCount - 1 WHERE s.id = :storeId AND s.reviewCount > 0")
+    void decreaseReviewCount(@Param("storeId") Long storeId);
 }
