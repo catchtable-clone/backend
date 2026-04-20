1f43c29f8a78604943eaac2cd0177bc50bbbb01a
silkair <silkair1234@gmail.com>
2026-04-20 14:29:03 +0900

    feat : chatbot exclude 삭제

--- src/main/java/com/catchtable/CatchtableApplication.java
+++ src/main/java/com/catchtable/CatchtableApplication.java
@@ -2,14 +2,8 @@ package com.catchtable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
-@EnableScheduling
-@SpringBootApplication(exclude = {
-    org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration.class
-})
+@SpringBootApplication
 public class CatchtableApplication {
 
     public static void main(String[] args) {
